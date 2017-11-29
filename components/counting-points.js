a8ce8b3148ec4df48c220119961887df1ec6eaa2
Florian Fehrenbacher <florian-fehrenbacher@gmx.de>
2017-11-29 15:08:03 +0100

    add earthquakes data to globe

--- components/counting-points.js
+++ components/counting-points.js
@@ -1,51 +1,56 @@
 AFRAME.registerComponent('map', {
   init: function () {
-	d3.csv("data/zaehlstellen-durchschnitt.csv", function(data) {
+  	d3.csv("data/zaehlstellen-durchschnitt.csv", function(data) {
 
-		var heightScale = d3.scale.linear()
-			.domain([0, d3.max(data, function(d) { return d.DTV_Kfz_MobisSo_Q; }) ])
-			.range([0,0.9])
+  		var heightScale = d3.scale.linear()
+  			.domain([d3.min(data, function(d) { return d.DTV_Kfz_MobisSo_Q; }), d3.max(data, function(d) { return d.DTV_Kfz_MobisSo_Q; }) ])
+  			.range([0,0.9])
 
-		var longitudeScale = d3.scale.linear()
-			.domain([5.8721,15.0409])
-			.range([0,10.213])
+  		var longitudeScale = d3.scale.linear()
+  			.domain([5.8721,15.0409])
+  			.range([0,10.213])
 
-		var latitudeScale = d3.scale.linear()
-			.domain([47.2691,55.0572])
-			.range([0,-8.647])
+  		var latitudeScale = d3.scale.linear()
+  			.domain([47.2691,55.0572])
+  			.range([0,-8.647])
 
-		var scene = d3.select("a-scene");
+  		var scene = d3.select("a-scene");
 
-		var cylinders = scene.selectAll("a-cylinder.cyl")
-			.data(data)
-			.enter()
-			.append("a-cylinder")
-			.classed("cyl",true)
-			.attr({
-				position: function(d) {
-					// returning x coordinate between 0 and 2
-					var x = longitudeScale(d.Koor_WGS84_E);
-					var z = latitudeScale(d.Koor_WGS84_N);
+  		var cylinders = scene.selectAll("a-cylinder.cyl")
+  			.data(data)
+  			.enter()
+  			.append("a-cylinder")
+  			.classed("cyl",true)
+  			.attr({
+  				position: function(d) {
+  					// returning x coordinate between 0 and 2
+  					var x = longitudeScale(d.Koor_WGS84_E);
+  					var z = latitudeScale(d.Koor_WGS84_N);
 
-					var y = heightScale(d.DTV_Kfz_MobisSo_Q) / 2;
+  					var y = heightScale(d.DTV_Kfz_MobisSo_Q) / 2;
 
-					return x + " " + y + " " + z
-				},
-				height: function(d) {
-					return heightScale(d.DTV_Kfz_MobisSo_Q)
-				},
-				radius: function(d) {
-					return 0.03
-				},
-				color: function(d) {
-					if(heightScale(d.DTV_Kfz_MobisSo_Q)<0.3)
-					return "rgb(0,255,0)"
-					if(heightScale(d.DTV_Kfz_MobisSo_Q)>0.6)
-					return "rgb(255,0,0)"
-					else
-					return "rgb(255,255,0)"
-				}
-			})
-	});
+  					return x + " " + y + " " + z
+  				},
+  				radius: function(d) {
+  					return 0.03
+  				},
+          height: '0',
+  				color: function(d) {
+  					if(heightScale(d.DTV_Kfz_MobisSo_Q)<0.3)
+  					return "rgb(0,255,0)"
+  					if(heightScale(d.DTV_Kfz_MobisSo_Q)>0.6)
+  					return "rgb(255,0,0)"
+  					else
+  					return "rgb(255,255,0)"
+  				}
+  			})
+        .transition()
+        .duration(10000)
+        .attr({
+          height: function(d) {
+            return heightScale(d.DTV_Kfz_MobisSo_Q)
+          }
+        });
+  	});
   }
 });
--- components/world.js
+++ components/world.js
@@ -1,10 +1,53 @@
 /* globals AFRAME */
 AFRAME.registerComponent('show-country-info', {
   init: function () {
-    this.el.addEventListener('geojson-feature-selected', this.setText);
-  },
+    d3.json("data/all-one-week.geo.json", function(worldData) {
+      var data = worldData.features;
 
-  setText: function (event) {
-    console.log(event.detail.feature);
+      var inradians = function (degrees) {
+        return degrees * (Math.PI / 180);
+      }
+      var magnitudeScale = d3.scale.linear()
+  			.domain([d3.min(data, function(d) { return d.properties.mag ; }), d3.max(data, function(d) { return d.properties.mag ; }) ])
+  			.range([0.2,1])
+
+      var globeRadius = 2;
+      var scene = d3.select("#target");
+      var earthquakes = scene.selectAll("a-cylinder")
+  			.data(data)
+  			.enter()
+  			.append("a-cylinder")
+  			.classed("earthquake",true)
+  			.attr({
+  				position: function(d) {
+  					// convert lat(geometry[1]) and long(geometry[0]) in cartesian coordinates
+  					var x = globeRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.cos(inradians(d.geometry.coordinates[0]));
+            var y = globeRadius * Math.sin(inradians(d.geometry.coordinates[1]))
+            var z = -(globeRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.sin(inradians(d.geometry.coordinates[0])))
+  					return x + " " + y + " " + z
+  				},
+  				height: function(d) {
+  					return magnitudeScale(d.properties.mag);
+  				},
+          rotation: function(d){
+            var xRot = - (90 - d.geometry.coordinates[1])
+            var yRot = - (90 - d.geometry.coordinates[0])
+            return xRot + " " + yRot + " 0"
+          },
+          radius: '0.02',
+          color: function(d){
+            return 'rgb(' + Math.floor(Math.random() * 255)
+                    + ',' + Math.floor(Math.random() * 255)
+                    + ',' + Math.floor(Math.random() * 255) + ')'
+          }
+        });
+    //this.el.addEventListener('geojson-feature-selected', this.setText);
+    });
   }
+/*
+  setText: function (event) {
+
+    //console.log(event.detail.feature);
+  }
+*/
 });
