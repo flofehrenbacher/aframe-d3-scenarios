6bd877ae253f1defba4634a82bf5247bdd008e0a
Florian Fehrenbacher <florian-fehrenbacher@gmx.de>
2017-12-04 14:33:54 +0100

    color representation for strength (red yellow green)

--- components/world.js
+++ components/world.js
@@ -1,15 +1,23 @@
 /* globals AFRAME */
 AFRAME.registerComponent('show-earthquakes', {
   init: function () {
-    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(worldData) {
+    // live data https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+    d3.json("../data/all-one-week.geo.json", function(worldData) {
       var data = worldData.features;
+      console.log("MIN: " + d3.min(data, function(d) { return d.properties.mag ; }));
+      console.log("MAX: " + d3.max(data, function(d) { return d.properties.mag ; }));
+
 
       var inradians = function (degrees) {
         return degrees * (Math.PI / 180);
       }
       var magnitudeScale = d3.scale.linear()
   			.domain([d3.min(data, function(d) { return d.properties.mag ; }), d3.max(data, function(d) { return d.properties.mag ; }) ])
-  			.range([0.2,1])
+  			.range([0.1,0.5])
+
+      var colorScale = d3.scale.linear()
+  			.domain([d3.min(data, function(d) { return d.properties.mag ; }), d3.max(data, function(d) { return d.properties.mag ; }) ])
+  			.range(['#FFF', '#0F0'])
 
       var globeRadius = 2;
       var scene = d3.select("#target");
@@ -20,10 +28,11 @@ AFRAME.registerComponent('show-earthquakes', {
   			.classed("earthquake",true)
   			.attr({
   				position: function(d) {
+            currentRadius = globeRadius + magnitudeScale(d.properties.mag)/2;
   					// convert lat(geometry[1]) and long(geometry[0]) in cartesian coordinates
-  					var x = globeRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.cos(inradians(d.geometry.coordinates[0]));
-            var y = globeRadius * Math.sin(inradians(d.geometry.coordinates[1]))
-            var z = -(globeRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.sin(inradians(d.geometry.coordinates[0])))
+  					var x = currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.cos(inradians(d.geometry.coordinates[0]));
+            var y = currentRadius * Math.sin(inradians(d.geometry.coordinates[1]))
+            var z = -(currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.sin(inradians(d.geometry.coordinates[0])))
   					return x + " " + y + " " + z
   				},
   				height: function(d) {
@@ -36,18 +45,22 @@ AFRAME.registerComponent('show-earthquakes', {
           },
           radius: '0.02',
           color: function(d){
+            /*
             return 'rgb(' + Math.floor(Math.random() * 255)
                     + ',' + Math.floor(Math.random() * 255)
                     + ',' + Math.floor(Math.random() * 255) + ')'
+            */
+            if(d.properties.mag < 2){
+              return '#0A0';
+            } else if (d.properties.mag < 5) {
+              return '#AA0';
+            } else if (d.properties.mag >= 5) {
+              return '#A00';
+            } else {
+              return '#00A';
+            }
           }
         });
-    //this.el.addEventListener('geojson-feature-selected', this.setText);
     });
   }
-/*
-  setText: function (event) {
-
-    //console.log(event.detail.feature);
-  }
-*/
 });
