8db12777f8ac2f49573bae9507974ec42a8cc456
Florian Fehrenbacher <florian-fehrenbacher@gmx.de>
2018-01-17 13:14:17 +0100

    add infobox and raycaster

--- components/world.js
+++ components/world.js
@@ -1,21 +1,24 @@
+// https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521
 /* globals AFRAME */
 AFRAME.registerComponent('show-earthquakes', {
   init: function () {
-    d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson', function(worldData) {
+    d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson', function(worldData) {
       var data = worldData.features;
-      console.log('MIN: ' + d3.min(data, function(d) { return d.properties.mag ; }));
-      console.log('MAX: ' + d3.max(data, function(d) { return d.properties.mag ; }));
 
       var inradians = function (degrees) {
         return degrees * (Math.PI / 180);
       }
+
+      var maxMag = d3.max(data, function(d) { return d.properties.mag ; });
+      var minMag = d3.min(data, function(d) { return d.properties.mag ; });
+
       var magnitudeScale = d3.scaleLinear()
-  			.domain([d3.min(data, function(d) { return d.properties.mag ; }), d3.max(data, function(d) { return d.properties.mag ; }) ])
-  			.range([0.1,0.5])
+  			.domain([minMag, maxMag])
+  			.range([0.1,0.5]);
 
       var colorScale = d3.scaleLinear()
-  			.domain([d3.min(data, function(d) { return d.properties.mag ; }), d3.max(data, function(d) { return d.properties.mag ; }) ])
-  			.range(['#FFF', '#F00'])
+  			.domain([minMag, (maxMag+minMag)/2 , maxMag])
+  			.range(['#0F0','#FF0','#F00']);
 
       var globeRadius = 2;
       var scene = d3.select('#target');
@@ -26,7 +29,11 @@ AFRAME.registerComponent('show-earthquakes', {
   			.append('a-cylinder')
   			.classed('earthquake',true)
   			.attrs({
+          height: function(d) {
+            return magnitudeScale(d.properties.mag);
+          },
   				position: function(d) {
+            // because height of cylinder grows up and down
             currentRadius = globeRadius + magnitudeScale(d.properties.mag)/2;
   					// convert lat(geometry[1]) and long(geometry[0]) in cartesian coordinates
   					var x = currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.cos(inradians(d.geometry.coordinates[0]));
@@ -34,36 +41,30 @@ AFRAME.registerComponent('show-earthquakes', {
             var z = -(currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.sin(inradians(d.geometry.coordinates[0])))
   					return x + ' ' + y + ' ' + z
   				},
-  				height: function(d) {
-  					return magnitudeScale(d.properties.mag);
-  				},
           rotation: function(d){
+            // cylinders stand on the surface in a right angle
             var xRot = - (90 - d.geometry.coordinates[1])
             var yRot = - (90 - d.geometry.coordinates[0])
             return xRot + ' ' + yRot + ' 0'
           },
-          radius: '0.02',
+          radius: '0.05',
           color: function(d){
-
-            return 'rgb(' + Math.floor(Math.random() * 255)
-                    + ',' + Math.floor(Math.random() * 255)
-                    + ',' + Math.floor(Math.random() * 255) + ')'
-
-            // create three categories of earthquake magnitude
-            /*
-            if(d.properties.mag < 2){
-              return '#0A0';
-            } else if (d.properties.mag < 5) {
-              return '#AA0';
-            } else if (d.properties.mag >= 5) {
-              return '#A00';
-            } else {
-              return '#00A';
-            }
-            */
-            // return colorScale(d.properties.mag)
+            return colorScale(d.properties.mag)
           }
         });
+
+      earthquakes
+        .on('raycaster-intersected', function (d) {
+          updateInformationBox(d);
+        });
     });
+
+    function updateInformationBox(d){
+      $.get('mustache/info-box.mst', function(template) {
+        var time = new Date(d.properties.time);
+        var rendered = Mustache.render(template, {d: d, time: time});
+        $('#info-box').html(rendered);
+      });
+    }
   }
 });
