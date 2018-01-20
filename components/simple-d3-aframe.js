b88edeb98d421dd62aa760c19a519e6604416f9b
Florian Fehrenbacher <florian-fehrenbacher@gmx.de>
2018-01-20 16:29:43 +0100

    make subtle changes and improve d3 code

--- components/simple-d3-aframe.js
+++ components/simple-d3-aframe.js
@@ -1,27 +1,19 @@
-/* globals AFRAME */
 AFRAME.registerComponent('simple-d3-aframe', {
   init: function () {
+    var heightScale = d3.scaleLinear()
+      .range([0,4]);
+    var colorScale = d3.scaleLinear()
+      .range(['red','orange','green']);
+
     d3.csv('data/simple-d3-aframe.csv', function(data) {
-      // convert strings to ints
-      data.forEach(function(d) {
-        d.numbersleft = +d.numbersleft;
-      });
-
-      var heightScale = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d){return d.numbersleft })])
-        .range([0,4]);
-
-      var colorScale = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d){return d.numbersleft })/2, d3.max(data, function(d){return d.numbersleft })])
-        .range(['red','orange','green']);
+      var maxNumbers = d3.max(data, function(d){ return d.numbersleft });
+      heightScale.domain([0, maxNumbers]);
+      colorScale.domain([0, maxNumbers/2, maxNumbers]);
 
       var scene = d3.select('a-scene');
-
-      var bars = scene.selectAll('a-cylinder.bar')
-        .data(data)
-        .enter()
+      var bars = scene.selectAll('a-cylinder')
+        .data(data).enter()
         .append('a-cylinder')
-        .classed('bar', true)
         .attrs({
           position: function(d,i) {
             var x = i*0.5 - 4;
@@ -37,7 +29,7 @@ AFRAME.registerComponent('simple-d3-aframe', {
             return colorScale(d.numbersleft)
           },
           shadow: ''
-        })
+        });
     });
   }
 });
