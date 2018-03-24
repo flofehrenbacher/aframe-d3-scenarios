0e3ce3b4857b65327b95b6fabce810beaa311a4c
Florian Fehrenbacher <florian-fehrenbacher@gmx.de>
2018-03-24 13:30:46 +0100

    dbscan working

---
+++ scenarios/interactive-globe/dbscan.js
@@ -0,0 +1,184 @@
+/*
+ Author: Corneliu S. (github.com/upphiminn)
+ 2013
+ */
+
+// obtained from https://github.com/upphiminn/jDBSCAN
+// modified to suit earthquake data in GeoJSON format by Florian Fehrenbacher
+
+(function() {
+    jDBSCAN = function() {
+        //Local instance vars.
+        var eps;
+        var minPts;
+        var data = [];
+        var clusters = [];
+        var status = [];
+        var distance = haversine_distance;
+
+
+        var dbscan = function() {
+            status = [];
+            clusters = [];
+
+            for (var i = 0; i < data.length; i++) {
+                if (status[i] === undefined) {
+                    status[i] = 0; //visited and marked as noise by default
+                    var neighbours = get_region_neighbours(i);
+                    var num_neighbours = neighbours.length;
+                    if (num_neighbours < minPts) {
+                        status[i] = 0; //noise
+                    } else {
+                        clusters.push([]); //empty new cluster
+                        var cluster_idx = clusters.length;
+                        expand_cluster(i, neighbours, cluster_idx);
+                    }
+                }
+            }
+
+            return status;
+        };
+
+        //Core Algorithm Related
+        function get_region_neighbours(point_idx) {
+            var neighbours = [];
+            var d = data[point_idx];
+
+            for (var i = 0; i < data.length; i++) {
+                if (point_idx === i) {
+                    continue;
+                }
+                if (distance(data[i], d) <= eps) {
+                    neighbours.push(i);
+                }
+            }
+
+            return neighbours;
+        }
+
+        function haversine_distance(point1, point2) {
+            // default 4 sig figs reflects typical 0.3% accuracy of spherical model
+            if (typeof precision === 'undefined') {
+                var precision = 4;
+            }
+
+            var R = 6371;
+            // lat: geometry[1] and long geometry[0]
+            var lat1 = point1.geometry.coordinates[1] * Math.PI / 180,
+                lon1 = point1.geometry.coordinates[0] * Math.PI / 180;
+            var lat2 = point2.geometry.coordinates[1] * Math.PI / 180,
+                lon2 = point2.geometry.coordinates[0] * Math.PI / 180;
+
+            var dLat = lat2 - lat1;
+            var dLon = lon2 - lon1;
+
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1) * Math.cos(lat2) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var d = R * c;
+
+            return d.toPrecision(precision);
+        }
+
+        function expand_cluster(point_idx, neighbours, cluster_idx) {
+            clusters[cluster_idx - 1].push(point_idx); //add point to cluster
+            status[point_idx] = cluster_idx; //assign cluster id
+
+            for (var i = 0; i < neighbours.length; i++) {
+                var curr_point_idx = neighbours[i];
+                if (status[curr_point_idx] === undefined) {
+                    status[curr_point_idx] = 0; //visited and marked as noise by default
+                    var curr_neighbours = get_region_neighbours(curr_point_idx);
+                    var curr_num_neighbours = curr_neighbours.length;
+                    if (curr_num_neighbours >= minPts) {
+                        expand_cluster(curr_point_idx, curr_neighbours, cluster_idx);
+                    }
+                }
+
+                if (status[curr_point_idx] < 1) { // not assigned to a cluster but visited (= 0)
+                    status[curr_point_idx] = cluster_idx;
+                    clusters[cluster_idx - 1].push(curr_point_idx);
+                }
+            }
+        }
+
+        //Resulting Clusters Center Points
+        dbscan.getClusters = function() {
+            var num_clusters = clusters.length;
+            var clusters_centers = [];
+
+            for (var i = 0; i < num_clusters; i++) {
+                if (distance === haversine_distance) {
+                    clusters_centers[i] = {
+                        location: {
+                            latitude: 0,
+                            longitude: 0
+                        }
+                    };
+                    clusters_centers[i].maxMagnitude = 0;
+                    clusters_centers[i].averageMagnitude = 0;
+                    clusters_centers[i].parts = [];
+                    for (var j = 0; j < clusters[i].length; j++) {
+                        clusters_centers[i].location.latitude += data[clusters[i][j]].geometry.coordinates[1];
+                        // longitude with minus value falsifies center point
+                        // converting it to positive value solves this problem
+                        if(data[clusters[i][j]].geometry.coordinates[0]<0)
+                            clusters_centers[i].location.longitude += data[clusters[i][j]].geometry.coordinates[0] + 360;
+                        else
+                            clusters_centers[i].location.longitude += data[clusters[i][j]].geometry.coordinates[0];
+                        // clusters_centers[i].parts.push(data[clusters[i][j]]);
+                        clusters_centers[i].averageMagnitude += data[clusters[i][j]].properties.mag;
+                        if (clusters_centers[i].maxMagnitude < data[clusters[i][j]].properties.mag)
+                            clusters_centers[i].maxMagnitude = data[clusters[i][j]].properties.mag;
+                    }
+
+                    clusters_centers[i].averageMagnitude /= clusters[i].length;
+                    clusters_centers[i].dimension = clusters[i].length;
+                    clusters_centers[i].parts = clusters[i];
+                    clusters_centers[i].location.latitude /= clusters[i].length;
+                    clusters_centers[i].location.longitude /= clusters[i].length;
+                }
+            }
+
+            return clusters_centers;
+        };
+
+        //Getters and setters
+        dbscan.data = function(d) {
+            if (arguments.length === 0) {
+                return data;
+            }
+            if (Array.isArray(d)) {
+                data = d;
+            }
+
+            return dbscan;
+        };
+
+        dbscan.eps = function(e) {
+            if (arguments.length === 0) {
+                return eps;
+            }
+            if (typeof e === "number") {
+                eps = e;
+            }
+
+            return dbscan;
+        };
+
+        dbscan.minPts = function(p) {
+            if (arguments.length === 0) {
+                return minPts;
+            }
+            if (typeof p === "number") {
+                minPts = p;
+            }
+
+            return dbscan;
+        };
+
+        return dbscan;
+    }
+})();
--- scenarios/interactive-globe/interactive-globe.js
+++ scenarios/interactive-globe/interactive-globe.js
@@ -1,11 +1,17 @@
-var currentComponent;
-var allEarthquakes;
+var thisComponent;
 var dataPointer;
-var minMag;
-var maxMag;
+
+var allEarthquakes;
+var clusteredEarthquakes;
+var globeRadius;
+
+var dbscanner;
+
 var magnitudeScale;
 var colorScale;
-var isBars = true;
+var radiusScale;
+
+var isOverview = true;
 
 AFRAME.registerComponent('show-earthquakes', {
     schema: {
@@ -14,145 +20,172 @@ AFRAME.registerComponent('show-earthquakes', {
         },
         radiusOfGlobe: {
             type: 'int'
-        },
-        numberOfEarthquakes: {
-            type: 'number'
-        },
-        filter: {
-            type: 'boolean'
         }
     },
     init: function() {
-        currentComponent = this;
+        thisComponent = this;
         dataPointer = this.data;
+        globeRadius = dataPointer.radiusOfGlobe;
 
         d3.json(dataPointer.src, function(earthquakes) {
             allEarthquakes = earthquakes.features;
 
-            if (dataPointer.numberOfEarthquakes) {
-                allEarthquakes = allEarthquakes.slice(0, dataPointer.numberOfEarthquakes);
-            }
+            dbscanner = jDBSCAN().eps(500).minPts(0).data(allEarthquakes);
+            console.log("START: " + performance.now());
+            dbscanner();
+            console.log("END:" + performance.now());
 
-            maxMag = d3.max(allEarthquakes, function(d) {
-                return d.properties.mag;
-            });
-            minMag = d3.min(allEarthquakes, function(d) {
-                return d.properties.mag;
-            });
+            // clusters of earthquakes
+            clusteredEarthquakes = dbscanner.getClusters();
+
+            // Scales for earthquakes
+            // numbers are hardcoded; experts can choose appropriate numbers
             magnitudeScale = d3.scaleLinear()
                 .domain([0, 7])
-                .range([0.02, 0.3]);
+                .range([0.05, 0.3]);
             colorScale = d3.scaleLinear()
                 .domain([1, 3.5, 6])
                 .range(['#0F0', '#FF0', '#F00']);
-            var currentData = allEarthquakes;
-            // first overview: filter to only show earthquakes with magnitude higher than 4
-            if(dataPointer.filter === true){
-                currentData = allEarthquakes.filter(currentComponent.checkMagnitude(3));
-            }
-            currentComponent.initBarsAsCylinders(currentData);
+
+            // additional scale for clusters' radius
+            radiusScale = d3.scaleLog()
+                .domain([1, 300, 100000])
+                .range([0.02, 0.2, 0.2])
+
+            thisComponent.initClusterAndDetailedView();
+            thisComponent.clusterOverview();
         });
     },
-    initBarsAsCylinders: function(data) {
-        d3.selectAll('a-cone').remove();
-        var globeRadius = dataPointer.radiusOfGlobe;
+    initClusterAndDetailedView: function() {
+        var scene = d3.select('#target');
+        var clusters = scene.selectAll('a-cylinder')
+            .data(clusteredEarthquakes)
+            .enter()
+            .append('a-cylinder')
+            .classed('earthquake', true)
+            .attrs({
+                height: function(d) {
+                    return magnitudeScale(d.averageMagnitude);
+                },
+                position: function(d) {
+                    // because height of cylinder grows up and down
+                    currentRadius = globeRadius + magnitudeScale(d.averageMagnitude) / 2;
+                    // convert lat(geometry[1]) and long(geometry[0]) in cartesian coordinates
+                    var x = currentRadius * Math.cos(inradians(d.location.latitude)) * Math.cos(inradians(d.location.longitude));
+                    var y = currentRadius * Math.sin(inradians(d.location.latitude))
+                    var z = -(currentRadius * Math.cos(inradians(d.location.latitude)) * Math.sin(inradians(d.location.longitude)))
+                    return x + ' ' + y + ' ' + z
+                },
+                rotation: function(d) {
+                    // cylinders stand on the surface in a right angle
+                    var xRot = -(90 - d.location.latitude)
+                    var yRot = -(90 - d.location.longitude)
+                    return xRot + ' ' + yRot + ' 0'
+                },
+                radius: function(d) {
+                    return radiusScale(d.dimension);
+                },
+                color: function(d) {
+                    return colorScale(d.maxMagnitude)
+                },
+                visible: false
+            });
 
+        clusters
+            .on('raycaster-intersected', function(d) {
+                console.log(d);
+            });
+
+            var earthquakes = scene.selectAll('a-cone')
+                .data(allEarthquakes)
+                .enter()
+                .append('a-cone')
+                .classed('earthquake', true)
+                .attrs({
+                    height: function(d) {
+                        return magnitudeScale(d.properties.mag);
+                    },
+                    position: function(d) {
+                        // because height of cylinder grows up and down radius has to be adjusted
+                        currentRadius = globeRadius + magnitudeScale(d.properties.mag) / 2;
+                        // convert latitude(geometry[1]) and longitude(geometry[0]) in cartesian coordinates
+                        var x = currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.cos(inradians(d.geometry.coordinates[0]));
+                        var y = currentRadius * Math.sin(inradians(d.geometry.coordinates[1]))
+                        var z = -(currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.sin(inradians(d.geometry.coordinates[0])))
+                        return x + ' ' + y + ' ' + z
+                    },
+                    rotation: function(d) {
+                        // cylinders stand on the surface in a right angle
+                        var xRot = -(90 - d.geometry.coordinates[1])
+                        var yRot = -(90 - d.geometry.coordinates[0])
+                        return xRot + ' ' + yRot + ' 0'
+                    },
+                    'radius-bottom': '0.02',
+                    'radius-top': '0',
+                    color: function(d) {
+                        return colorScale(d.properties.mag)
+                    },
+                    visible: false
+                });
+    },
+    clusterOverview: function() {
+        $('#info-box').fadeOut();
         var inradians = function(degrees) {
             return degrees * (Math.PI / 180);
         }
 
         var scene = d3.select('#target');
+        scene.selectAll('a-cone')
+            .attr('visible', false);
 
+        scene.selectAll('a-cylinder')
+            .attr('visible', true);
+
+    },
+    detailedView: function() {
+        d3.selectAll('a-cylinder');
+        var scene = d3.select('#target');
+        $('#info-box').fadeIn();
+
+        scene.selectAll('a-cylinder')
+            .attr('visible', false);
         var earthquakes = scene.selectAll('a-cone')
-            .data(data)
-            .enter()
-            .append('a-cone')
-            .classed('earthquake', true)
-            .attrs({
-                height: function(d) {
-                    return magnitudeScale(d.properties.mag);
-                },
-                position: function(d) {
-                    // because height of cylinder grows up and down
-                    currentRadius = globeRadius + magnitudeScale(d.properties.mag) / 2;
-                    // convert lat(geometry[1]) and long(geometry[0]) in cartesian coordinates
-                    var x = currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.cos(inradians(d.geometry.coordinates[0]));
-                    var y = currentRadius * Math.sin(inradians(d.geometry.coordinates[1]))
-                    var z = -(currentRadius * Math.cos(inradians(d.geometry.coordinates[1])) * Math.sin(inradians(d.geometry.coordinates[0])))
-                    return x + ' ' + y + ' ' + z
-                },
-                rotation: function(d) {
-                    // cylinders stand on the surface in a right angle
-                    var xRot = -(90 - d.geometry.coordinates[1])
-                    var yRot = -(90 - d.geometry.coordinates[0])
-                    return xRot + ' ' + yRot + ' 0'
-                },
-                'radius-bottom': '0.05',
-                'radius-top': '0.05',
-                color: function(d) {
-                    return colorScale(d.properties.mag)
-                }
-            });
+            .attr('visible', true);
 
         earthquakes
             .on('raycaster-intersected', function(d) {
                 updateInformationBox(d);
             });
-
-        function updateInformationBox(d) {
-            $.get('mustache/info-box.mst', function(template) {
-                var time = new Date(d.properties.time);
-                var rendered = Mustache.render(template, {
-                    d: d,
-                    time: time
-                });
-                $('#info-box').html(rendered);
-            });
-        }
-    },
-    changeToCones: function() {
-        var scene = d3.select('#target');
-        $('#info-box').fadeIn();
-        var earthquakes = scene.selectAll('a-cone')
-            .attrs({
-                'radius-bottom': '0.01',
-                'radius-top': '0'
-            });
-    },
-    changeToBars: function() {
-        var scene = d3.select('#target');
-        $('#info-box').fadeOut();
-        var earthquakes = scene.selectAll('a-cone')
-            .attrs({
-                'radius-bottom': '0.05',
-                'radius-top': '0.05',
-            });
     },
     tick: function() {
-        var earth = this.el.components.position.attrValue;
-        var camera = this.el.sceneEl.querySelector("#camera").components.position.attrValue;
+        var earth = thisComponent.el.components.position.attrValue;
+        var camera = thisComponent.el.sceneEl.querySelector("#camera").components.position.attrValue;
         var distance = Math.sqrt(Math.pow(earth.x - camera.x, 2) +
             Math.pow(earth.y - camera.y, 2) +
             Math.pow(earth.z - camera.z, 2));
-        if (distance < 3 && isBars === true) {
-            this.initBarsAsCylinders(allEarthquakes);
-            this.changeToCones();
-            isBars = false;
+        if (distance < 3 && isOverview === true) {
+            thisComponent.detailedView();
+            isOverview = false;
         }
-        if (distance >= 3 && isBars === false) {
-            var temp = allEarthquakes;
-            // first overview: filter to only show earthquakes with magnitude higher than 4
-            if(dataPointer.filter === true){
-                temp = allEarthquakes.filter(currentComponent.checkMagnitude(3));
-            }
-            this.initBarsAsCylinders(temp);
-            this.changeToBars();
-            isBars = true;
-        }
-    },
-    checkMagnitude: function(number) {
-        return function(d) {
-            return d.properties.mag > number;
+        if (distance >= 3 && isOverview === false) {
+            thisComponent.clusterOverview();
+            isOverview = true;
         }
     }
 });
+
+// HELPER METHODS
+function updateInformationBox(d) {
+    $.get('mustache/info-box.mst', function(template) {
+        var time = new Date(d.properties.time);
+        var rendered = Mustache.render(template, {
+            d: d,
+            time: time
+        });
+        $('#info-box').html(rendered);
+    });
+}
+
+function inradians(degrees) {
+    return degrees * (Math.PI / 180);
+}
